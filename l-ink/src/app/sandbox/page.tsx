8fb484ecb25a90b8a7f9c9a2a1628bdd858e695d
Chris Wurst <cwurst@uoregon.edu>
2024-03-12 22:42:51 -0700

    add r3f canvas

--- l-ink/src/app/sandbox/page.tsx
+++ l-ink/src/app/sandbox/page.tsx
@@ -1,3 +1,70 @@
+'use client';
+
+import { Canvas } from "@react-three/fiber"
+import { DoubleSide } from "three";
+
 export default function Page() {
-    return <></>
+    const positions = new Float32Array([
+        1, 0, 0,
+        0, 1, 0,
+        -1, 0, 0,
+        0, -1, 0
+    ])
+    const normals = new Float32Array([
+        0, 0, 1,
+        0, 0, 1,
+        0, 0, 1,
+        0, 0, 1,
+    ])
+    const colors = new Float32Array([
+        1, 1, 1, 1,
+        1, 1, 1, 1,
+        1, 1, 1, 1,
+        0, 1, 0, 1,
+    ])
+    const indices = new Uint16Array([
+        0, 1, 3,
+        2, 3, 1,
+    ])
+    const vertexCount = positions.length/3;
+
+    return (
+        <Canvas>
+            <ambientLight />
+            <mesh>
+                <bufferGeometry>
+                    <bufferAttribute
+                        attach='attributes-position'
+                        array = {positions}
+                        count={vertexCount}
+                        itemSize={3}
+                    />
+                    <bufferAttribute
+                        attach='attributes-normals'
+                        array = {normals}
+                        count={vertexCount}
+                        itemSize={3}
+                    />
+                    <bufferAttribute
+                        attach='attributes-color'
+                        array = {colors}
+                        count={vertexCount}
+                        itemSize={4}
+                    />
+                    <bufferAttribute
+                        attach='index'
+                        array = {indices}
+                        count={indices.length}
+                        itemSize={1}
+                    />
+                </bufferGeometry>
+                <meshStandardMaterial 
+                    vertexColors
+                />
+            </mesh>
+            {/* <mesh>
+                <boxGeometry />
+            </mesh> */}
+        </Canvas>
+    )
 }
