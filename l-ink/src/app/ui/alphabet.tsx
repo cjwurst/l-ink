a586b0e12bdff9e332b733962f2b6fa77f198dc2
Chris Wurst <cwurst@uoregon.edu>
2024-03-14 11:53:54 -0700

    add alphabet input to the side config

---
+++ l-ink/src/app/ui/alphabet.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+
+export default function Alphabet() {
+    const searchParams = useSearchParams();
+    const pathname = usePathname();
+    const { replace } = useRouter();
+
+    const reservedChars:string[] = ["|", ":"];
+
+    function handleChange(term: string) {
+        const params = new URLSearchParams(searchParams);
+        const chars = Array.from(term);
+        console.log(chars);
+        const alphabet = chars.filter((c, i) => { 
+            return chars.indexOf(c) === i &&           // remove duplicates
+            reservedChars.indexOf(c) === -1     // remove reserved characters
+        }).join("");        
+        console.log(alphabet);
+        if (term) {
+            params.set('alphabet', alphabet);
+        } else {
+            params.delete('alphabet');
+        }
+        replace(`${pathname}?${params.toString()}`);
+    }
+
+    return (
+        <div className="relative flex pt-5 flex-grow">
+            <div className="w-24">
+                <label htmlFor="alphabet"> Alphabet: </label>
+            </div>
+            <input 
+                id="alphabet" 
+                className="peer block outline-2 w-full border border-slate-500 bg-slate-200 text-black" 
+                onChange={ (e) => { handleChange(e.target.value); }}
+                defaultValue = {searchParams.get('alphabet')?.toString()}
+            />
+        </div>
+    );
+}
--- l-ink/src/app/ui/sideconfig.tsx
+++ l-ink/src/app/ui/sideconfig.tsx
@@ -1,4 +1,4 @@
-import { useParams, useRouter, usePathname } from 'next/navigation';
+import Alphabet from '@/app/ui/alphabet';
 import Axiom from '@/app/ui/axiom';
 import Ruleset from '@/app/ui/ruleset';
 import IterateButton from '@/app/ui/renderButton';
@@ -7,6 +7,7 @@ export default function SideConfig() {
     return (
         <div className="h-full flex-col items-center justify-start text-slate-300">
             <div className="pl-5">
+                <Alphabet />
                 <Axiom />
                 <Ruleset />
                 <div className="mt-5 flex flex-col items-center bg-slate-800">
