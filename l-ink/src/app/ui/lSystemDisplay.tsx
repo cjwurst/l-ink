7c22be8f594e74c609f34aa67afd9e0df9b495d5
Chris Wurst <cwurst@uoregon.edu>
2024-03-13 21:33:07 -0700

    remove unnecessary imports

--- l-ink/src/app/ui/lSystemDisplay.tsx
+++ l-ink/src/app/ui/lSystemDisplay.tsx
@@ -2,7 +2,6 @@
 
 import { Canvas } from '@react-three/fiber'
 import DrawInstruction from '@/app/lib/drawInstruction';
-import { ReactNode } from 'react';
 import { Line } from '@react-three/drei';
 
 type LSystemProps = {
