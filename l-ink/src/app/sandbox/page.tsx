6128d782a30f34374277cacc8db9f042e2c4f5d6
Chris Wurst <cwurst@uoregon.edu>
2024-03-22 18:20:44 -0700

    move pathname param reading to page

--- l-ink/src/app/sandbox/page.tsx
+++ l-ink/src/app/sandbox/page.tsx
@@ -1,9 +1,43 @@
-'use client';
+"use client";
 
 import LSystem from "@/app/ui/lSystem";
+import { useSearchParams } from "next/navigation";
+import DrawInstruction from "../lib/drawInstruction";
 
 export default function Page() {
-    return <div className="bg-slate-500">
-        <LSystem />
-    </div>
+    const searchParams = useSearchParams();
+
+    function getAlphabetString(): string {
+        return searchParams.get("alphabet")?.toString() || "";
+    }
+
+    function getIterateRules(): Map<string, string> {
+        let result: Map<string, string> = new Map();
+        const rules = searchParams.get("iterate")?.toString() || "";
+        for (let i = 0; i < rules.length; i++) {
+            const rule = rules[i].split(":");
+            result.set(rule[0], rule[1]);
+        }
+        return result;
+    }
+
+    function getDrawRules(): Map<string, DrawInstruction> {
+        let result: Map<string, DrawInstruction> = new Map();
+        const rules = searchParams.get("draw")?.toString() || "";
+        for (let i = 0; i < rules.length; i++) {
+            const rule = rules[i].split(":");
+            result.set(rule[0], rule[1] as DrawInstruction);
+        }
+        return result;
+    }
+    
+    return (
+        <div className="bg-slate-800">
+            <LSystem 
+                defaultAlphabet={getAlphabetString()}
+                defaultIterateRules={getIterateRules()}
+                defaultDrawRules={getDrawRules()}
+            />
+        </div>
+    );
 }
