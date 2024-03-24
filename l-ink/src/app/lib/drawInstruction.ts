a6232f18d42d24412345c62e5eb04a3b4f6711ef
Chris Wurst <cwurst@uoregon.edu>
2024-03-24 13:07:52 -0700

    add DrawInstruction encoding for shorter URLs

--- l-ink/src/app/lib/drawInstruction.ts
+++ l-ink/src/app/lib/drawInstruction.ts
@@ -5,3 +5,14 @@ enum DrawInstruction {
 }
 
 export default DrawInstruction;
+
+//characters representing each instruction in copied URL
+const encode = new Map([
+    [DrawInstruction.FORWARD, "a"],
+    [DrawInstruction.TURN_LEFT, "b"],
+    [DrawInstruction.TURN_RIGHT, "c"]
+]);
+const decode = new Map(Array.from(encode).map(([k, v]) => [v, k]));
+
+export const encodeDrawInstruction = (d:DrawInstruction) => encode.get(d);
+export const decodeDrawInstruction = (c:string) => decode.get(c);
