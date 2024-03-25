428f7f34100d9695120395187efe93fcc0695487
cwurst <cwurst@uoregon.edu>
2024-03-25 13:19:12 -0700

    remove old landing page

--- l-ink/src/app/page.tsx
+++ l-ink/src/app/page.tsx
@@ -1,12 +1,6 @@
-import { ruleStringToMap, ruleMapToString } from "@/app/lib/lSystemHelpers";
-
 export default function Home() {
     const ruleString = "a:aa|b:ab|c:a";
     return (
-        <div className="flex flex-col h-screen bg-gray-800 text-slate-100 justify-center items-center text-xl">
-            <div>Rulestring: {`${ruleString}`}</div>
-            <div>Map: {`${ruleStringToMap(ruleString)}`}</div>
-            <div>Back to string: {`${ruleMapToString(ruleStringToMap(ruleString))}`}</div>
-        </div>
+        <></>
     );
 }
