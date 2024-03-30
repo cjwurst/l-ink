d76c560eb31614d416591e03e0d7a95c061165e7
cwurst <cwurst@uoregon.edu>
2024-03-29 20:14:18 -0700

    remove right ruleset padding

--- l-ink/src/app/ui/ruleset.tsx
+++ l-ink/src/app/ui/ruleset.tsx
@@ -31,7 +31,7 @@ export default function Ruleset({alphabet, iterateRules, onChangeIterateRules, d
         <div className="flex flex-col gap-4">
             <div className="block text-sm font-medium text-gray-900 dark:text-white"> Rules: </div>
             {getAlphabetArray().map((preimage) => (
-                <div className="grid grid-cols-[repeat(3,auto)] gap-4 w-full pl-4 pr-4">
+                <div className="grid grid-cols-[repeat(3,auto)] gap-4 w-full pl-4">
                     <select 
                         className = "text-sm font-medium rounded-lg bg-gray-50 border border-gray-300 text-gray-900 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         onChange={(e)=> handleChangeDrawRule(preimage, e.target.value as DrawInstruction)}
