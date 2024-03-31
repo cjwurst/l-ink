072a32706bc1006697e373b30a4e05f8932bba14
cwurst <cwurst@uoregon.edu>
2024-03-31 15:41:00 -0700

    disable scrolling on numeric inputs

--- l-ink/src/app/ui/configInput.tsx
+++ l-ink/src/app/ui/configInput.tsx
@@ -23,6 +23,9 @@ export default function ConfigInput({onChange, value, name, type="text", step=1}
                 id={name} 
                 className="block bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
                 onChange={(e) => { onChange(e.target.value || "");}}
+                onWheel={(e) => {
+                    if (e.target instanceof HTMLElement) e.target.blur();
+                }}
                 value = {value}
             />
         </div>
