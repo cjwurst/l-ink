caecf890fe8f9421af9fe171475d2cd70b6d4c60
cwurst <cwurst@uoregon.edu>
2024-04-01 15:28:52 -0700

    fix list key warning

--- l-ink/src/app/ui/ruleset.tsx
+++ l-ink/src/app/ui/ruleset.tsx
@@ -31,8 +31,9 @@ export default function Ruleset({alphabet, iterateRules, onChangeIterateRules, d
         <div className="flex flex-col gap-4">
             <div className="block text-sm font-medium text-gray-900 dark:text-white"> Rules: </div>
             {getAlphabetArray().map((preimage) => (
-                <div className="grid grid-cols-[repeat(3,auto)] gap-4 w-full pl-4">
+                <div key={`${preimage}-rule`} className="grid grid-cols-[repeat(3,auto)] gap-4 w-full pl-4">
                     <select 
+                        key={`${preimage}-select`}
                         className = "text-sm font-medium rounded-lg bg-gray-50 border border-gray-300 text-gray-900 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         onChange={(e)=> handleChangeDrawRule(preimage, e.target.value as DrawInstruction)}
                         value={drawRules.get(preimage) || DrawInstruction.FORWARD}
@@ -46,19 +47,12 @@ export default function Ruleset({alphabet, iterateRules, onChangeIterateRules, d
                         {preimage} ↦
                     </div>
                     <input 
+                        key={`${preimage}-input`}
                         className = "block bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         onChange={(e) => handleChangeImage(preimage, e.target.value)}
                         value={iterateRules.get(preimage) || ""}
                     />
                 </div>
-                // <Rule
-                //     key={preimage}
-                //     preimage={preimage}
-                //     image={iterateRules.get(preimage) || ""}
-                //     drawRule={drawRules.get(preimage) || DrawInstruction.FORWARD}
-                //     onChangeImage={(image: string) => handleChangeImage(preimage, image)}
-                //     onChangeDrawRule={(drawRule: DrawInstruction) => handleChangeDrawRule(preimage, drawRule)}
-                // />
             ))}
         </div>
     );
