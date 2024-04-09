68357ad6fb671cb5b37ca73b9898f4b50b2f687f
cwurst <cwurst@uoregon.edu>
2024-04-09 13:50:28 -0700

    clean up

--- l-ink/src/app/(pages)/sandbox/page.tsx
+++ l-ink/src/app/(pages)/sandbox/page.tsx
@@ -7,24 +7,16 @@ import { decodeDrawInstruction } from "../../lib/drawInstruction";
 import URLCharacter from "../../lib/urlCharacter";
 import URLParamName from "@/app/lib/urlParamNames";
 import urlParamsToProps from "@/app/lib/urlParamReader";
+import { CameraControls, OrbitControls } from "@react-three/drei";
+import { MOUSE } from "three";
+import KochCameraControls from "@/app/ui/kochCameraControls";
 
 export default function Page() {
     const searchParams = useSearchParams();
 
     return (
         <div className="h-screen">
-            <LSystem {...urlParamsToProps(searchParams)}/>
-            {/* <LSystem 
-                defaultIterationCount={getNumberParam(URLParamName.ITERATION_COUNT)}
-                defaultAxiom={getStringParam(URLParamName.AXIOM)}
-                defaultAlphabet={getStringParam(URLParamName.ALPHABET)}
-                defaultIterateRules={getIterateRules()}
-                defaultDrawRules={getDrawRules()}
-                defaultAngle={getNumberParam(URLParamName.INITIAL_ANGLE)}
-                defaultAngleIncrement={getNumberParam(URLParamName.ANGLE_INCREMENT, 45)}
-                defaultOrigin={getOrigin()}
-                defaultDrawDistance={getNumberParam(URLParamName.DRAW_DISTANCE, 1)}
-            /> */}
+            <LSystem {...urlParamsToProps(searchParams)} />
         </div>
     );
 }
