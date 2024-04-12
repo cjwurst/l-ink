6e082197f2ec240bda177a85d6162b52eb289df5
cwurst <cwurst@uoregon.edu>
2024-04-12 12:19:34 -0700

    clean up

--- l-ink/src/app/ui/fitCameraControls.tsx
+++ l-ink/src/app/ui/fitCameraControls.tsx
@@ -1,6 +1,5 @@
 import { useThree } from "@react-three/fiber"
 import { useMemo } from "react";
-import { Vector3 } from "three";
 
 type FitCameraControlsProps = {
     xBounds: [number, number],
@@ -14,7 +13,6 @@ export default function FitCameraControls({xBounds, yBounds}: FitCameraControlsP
     useMemo(() => {
         const margin = 0.1;
         const center = [(xBounds[0]+xBounds[1])/2, (yBounds[0]+yBounds[1])/2];
-        const direction = new Vector3();
         const meshSize = [xBounds[1]-xBounds[0], yBounds[1]-yBounds[0]];
         camera.zoom = (1-margin)*Math.min(
             canvasSize.width/(meshSize[0]), 
@@ -23,7 +21,6 @@ export default function FitCameraControls({xBounds, yBounds}: FitCameraControlsP
         camera.position.set(center[0], center[1], 10);
         camera.lookAt(center[0], center[1], 0);
         camera.updateProjectionMatrix();
-        camera.getWorldDirection(direction);
     }, [camera, canvasSize, xBounds, yBounds]);
 
     return <></>
