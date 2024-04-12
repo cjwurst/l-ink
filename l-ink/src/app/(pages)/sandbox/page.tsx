13bf7324d42e15085a03717ac9fb3bf36aa5d249
cwurst <cwurst@uoregon.edu>
2024-04-12 13:44:08 -0700

    cleanup

--- l-ink/src/app/(pages)/sandbox/page.tsx
+++ l-ink/src/app/(pages)/sandbox/page.tsx
@@ -2,21 +2,14 @@
 
 import LSystem from "@/app/ui/lSystem";
 import { useSearchParams } from "next/navigation";
-import DrawInstruction from "../../lib/drawInstruction";
-import { decodeDrawInstruction } from "../../lib/drawInstruction";
-import URLCharacter from "../../lib/urlCharacter";
-import URLParamName from "@/app/lib/urlParamNames";
 import urlParamsToProps from "@/app/lib/urlParamReader";
-import { CameraControls, OrbitControls } from "@react-three/drei";
-import { MOUSE } from "three";
-import KochCameraControls from "@/app/ui/kochCameraControls";
 
 export default function Page() {
     const searchParams = useSearchParams();
 
     return (
         <div className="h-screen">
-            <LSystem {...urlParamsToProps(searchParams)} />
+            <LSystem {...urlParamsToProps(searchParams)} enableZoom />
         </div>
     );
 }
