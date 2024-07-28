8ee395d4fbea8dd4ff56d5a750a0b524c4350b1a
Chris Wurst <cwurst@uoregon.edu>
2024-07-27 23:19:31 -0700

    add suspense boundary to sandbox

--- l-ink/src/app/(pages)/sandbox/page.tsx
+++ l-ink/src/app/(pages)/sandbox/page.tsx
@@ -3,8 +3,17 @@
 import LSystem from "@/app/ui/lSystem";
 import { useSearchParams } from "next/navigation";
 import urlParamsToProps from "@/app/lib/urlParamReader";
+import { Suspense } from "react";
 
 export default function Page() {
+    return (
+        <Suspense>
+            <SuspenseBoundary />
+        </Suspense>
+    )
+}
+
+function SuspenseBoundary() {
     const searchParams = useSearchParams();
 
     return (
